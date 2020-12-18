4f4b6a02f0571b63f176c1ccc23ab39f6d72d595
iamreynold <reynolds.ottawa@gmail.com>
2020-12-18 18:16:23 -0500

    Support TFT screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,51 @@
 #include <Arduino.h>
 
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_ST7735.h>
+
+
+//TFT display
+#define TFT_TCS       16
+#define TFT_RST       15
+#define TFT_DC        14
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_TCS, TFT_DC, TFT_RST);
+
+#define BLINK_LED     8
+
+int blinkLedState         = LOW;
+unsigned long blinkMills  = 0;
+const long blinkInterval = 1000;
+
+
 void setup() {
-  pinMode(8,  OUTPUT); // Blue
-  pinMode(9,  OUTPUT); // Blue
-  pinMode(10, OUTPUT); // Red
+  //TFT initalization
+  tft.initR(INITR_144GREENTAB);
+  tft.fillScreen(ST7735_BLACK);
+  
+
+  // Blink Pin
+  pinMode(BLINK_LED, OUTPUT);
 }
 
 void loop() {
-  for (int i = 8; i <= 10; i++) {
-    digitalWrite(i, HIGH);
-    delay(200);
-    digitalWrite(i, LOW);
+  unsigned long loopMills = millis();
+
+  tft.setCursor(0, 0);
+  tft.setTextColor(0x07E0);
+  tft.setTextWrap(true);
+  tft.print("hello world" );
+
+
+  if (loopMills - blinkMills >= blinkInterval) {
+    blinkMills = loopMills;
+    if (blinkLedState == LOW) {
+      blinkLedState = HIGH;
+    } else {
+      blinkLedState = LOW;
+    }
+    digitalWrite(BLINK_LED, blinkLedState);
   }
-}
+
+ }
